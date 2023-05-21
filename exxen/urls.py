4ca36f09b6295e4a8bac8ca0e7d4ea060e75ca7f
aebedir <ae.bedirr@gmail.com>
2023-05-22 02:35:33 +0300

    üye giriş, kayıt ol, çıkış yap, şifre kriterleri

--- exxen/urls.py
+++ exxen/urls.py
@@ -23,5 +23,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('movies.urls'))
+    path('',include('movies.urls')),
+    path('user/',include('user.urls'))
 ] + static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
-
+from django.shortcuts import render ,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
 # Create your views here.
+
+
+def userRegister(request):
+    if request.method == 'POST':
+        username = request.POST['kullanici']
+        email = request.POST['email']
+        sifre1 = request.POST['password1']
+        sifre2= request.POST['password2']
+
+        if sifre1 == sifre2:
+            if User.objects.filter(username = username).exists():
+                messages.error(request,'Kullanıcı adı kullanımda !')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request,'Email daha önceden kullanılmış !')
+            elif len(sifre1) < 6:
+                messages.error(request,'Şifre en az 6 karakter olmalı !')
+            elif username.lower() in sifre1.lower():
+                messages.error(request,'Kullanıcı adı ile şifre benzer olamaz !')
+            elif '.' not in sifre1:
+                messages.error(request,'Şifre nokta içermelidir !')
+            else:
+                user = User.objects.create_user(
+                    username = username,
+                    email = email,
+                    password = sifre1
+                )
+                user.save()
+                messages.success(request,'Kayıt Başarılı')
+                return redirect('login')
+        else:
+            messages.error(request,'Şifreler uyuşmuyor !')
+            
+    return render(request,'user/register.html')
+
+
+def userLogin(request):
+    if request.method == 'POST':
+        kullanici = request.POST['kullanici']
+        sifre = request.POST['password']
+
+        user = authenticate(request, username = kullanici , password = sifre)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request,'Giriş Yapıldı')
+            return redirect('index')
+        else:
+            messages.error(request,'Kullanıcı adı veya şifre hatalı')
+    return render(request,'user/login.html')
+
+def userLogout(request):
+    logout(request)
+    messages.success(request,'Çıkış Yapıldı')
+    return redirect('index')
